18aa9effd7a9d529830e27eae86a842149cd57ad
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2020-05-31 09:23:23 +0300

    feat(utils): utils wrapper
    Utils wrapper which will contain helper methods & functions to be used by other components of the
    system

---
+++ utils.py
@@ -0,0 +1,24 @@
+"""
+Utils
+
+Will contain common utility or helper functions that will be re-useable and used across the system
+"""
+from socket import socket
+
+def communicate(host, port, request):
+    """
+    Handles communication with remote sockets providing the host port & request 
+    to send to a remote socket connection.
+    Returns response as received from remote socket
+    :param host: Host address
+    :param port: Port to connect to
+    :param request: Request to send to remote host
+    :return: Returns response in form of bytes with a buffer size of 1024 as recevied from host connection
+    :rtype: bytes
+    """
+    s = socket()
+    s.connect((host, port))
+    s.send(request)
+    response = s.recv(1024)
+    s.close()
+    return response
